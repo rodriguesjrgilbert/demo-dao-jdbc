711c52911f56d23d02e45529ca9f8907a0f10967
Gilberto Junior Rodrigues <gilberto.junior@outlook.com>
2020-05-26 09:48:09 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg Majorie", "gmajorie@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		System.out.println("\n==== TEST 5: seller update ====");
+		seller = sellerDao.findById(7);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Updated completed!");		
 	}
 }
